910721371a814d71cd7da18f5a0051cf99e756e6
Sofia Palladino <166324582+SofiaIPalladino@users.noreply.github.com>
2025-10-20 18:48:35 -0300

    Add MockV3Aggregator

--- Tercera Entrega/contracts/MockV3Aggregator.sol
+++ Tercera Entrega/contracts/MockV3Aggregator.sol
@@ -1,1 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink/v1.11.0/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract MockV3Aggregator is AggregatorV3Interface {
+    uint8 private _decimals;
+    int256 private _answer;
+    uint80 private _roundId;
+    uint256 private _updatedAt;
+
+    constructor(uint8 decimals_, int256 initialAnswer) {
+        _decimals = decimals_;
+        _answer = initialAnswer;
+        _roundId = 1;
+        _updatedAt = block.timestamp;
+    }
+
+    function decimals() external view override returns (uint8) {
+        return _decimals;
+    }
+
+    function description() external pure override returns (string memory) {
+        return "MockV3Aggregator";
+    }
+
+    function version() external pure override returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80 ) external view override returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
+        return (_roundId, _answer, _updatedAt, _updatedAt, _roundId);
+    }
+
+    function latestRoundData() external view override returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
+        return (_roundId, _answer, _updatedAt, _updatedAt, _roundId);
+    }
+
+    // helpers for tests
+    function updateAnswer(int256 newAnswer) external {
+        _answer = newAnswer;
+        _roundId += 1;
+        _updatedAt = block.timestamp;
+    }
+}
